7fb2445457032978eda3fecbf66c7531a0240af0
Alen <info@alenbarac.com>
2023-06-29 01:18:33 +0200

    api helper http fix

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://api-reports.fishinhole-integration.commer.com/api',
+  baseURL: 'http://localhost:8000/api',
   withCredentials: true,
-});
+})
 
-export default apiClient;
+export default apiClient
